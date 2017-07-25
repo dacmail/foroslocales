6e29958df7a21d1c25fa1478a4ff1e53e5b4657d
Daniel Aguilar <dacmail@gmail.com>
2017-07-25 11:02:58 +0200

    listado de eventos

--- wp-content/themes/ffll-foro/index.php
+++ wp-content/themes/ffll-foro/index.php
@@ -6,6 +6,7 @@
     <div class="row">
       <div class="col-sm-6">
         <h2 class="section-title">Calendario. <?= Extras\ungrynerd_svg('icon-calendar'); ?></h2>
+        <?php em_events(array('limit' => 3)); ?>
       </div>
       <div class="col-sm-6">
         <?php em_calendar(array('format_header' => '')); ?>
