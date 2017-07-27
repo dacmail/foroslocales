b1384d35487c66e1d1a366e5fb240fa67fa4824d
Daniel Aguilar <dacmail@gmail.com>
2017-07-27 12:53:35 +0200

    Remove remote backups

--- wp-content/remote-backup-1500634764.sql
+++
@@ -1,9 +0,0 @@
--- Created at 21.7.2017 11:14 using David Grudl MySQL Dump Utility
--- Host: foroslocales.ungrynerd.com
--- MySQL Server: 5.6.28-76.1
--- Database: foroslocales
-
-SET NAMES utf8;
-SET SQL_MODE='NO_AUTO_VALUE_ON_ZERO';
-SET FOREIGN_KEY_CHECKS=0;
--- THE END
