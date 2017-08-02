4314ea24de1ce03f7b99f4e1443f9c44d4f8d203
Daniel Aguilar <dacmail@gmail.com>
2017-08-02 13:45:54 +0200

    list news

--- wp-content/themes/ffll-foro/index.php
+++ wp-content/themes/ffll-foro/index.php
@@ -1,3 +1,27 @@
 <?php use Roots\Sage\Extras; ?>
 
-INDEX
+<section class="posts-list">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4">
+        <h2 class="section-title">Noticias. <?= Extras\ungrynerd_svg('icon-news'); ?></h2>
+      </div>
+      <div class="col-md-8">
+        <?php while (have_posts()) : the_post(); ?>
+          <article class="post post--listing">
+            <h2 class="post__title">
+              <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+            </h2>
+            <p class="post__date"><?php the_time(get_option('date_format')); ?></p>
+            <?php the_excerpt(); ?>
+            <div class="post__tags">
+              <?= Extras\ungrynerd_svg('icon-tag'); ?>
+              <?php the_tags('', ', '); ?>
+            </div>
+          </article>
+        <?php endwhile; ?>
+        <?= Extras\ungrynerd_pagination(); ?>
+      </div>
+    </div>
+  </div>
+</section>
