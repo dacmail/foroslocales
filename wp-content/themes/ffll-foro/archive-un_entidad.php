37563115b4e688132007e5a5a3019c38a7dba17d
Daniel Aguilar <dacmail@gmail.com>
2017-08-14 10:25:20 +0200

    Link de entidades

--- wp-content/themes/ffll-foro/archive-un_entidad.php
+++ wp-content/themes/ffll-foro/archive-un_entidad.php
@@ -9,7 +9,16 @@
       <div class="col-md-8">
         <ul class="entidades">
           <?php while (have_posts()) : the_post(); ?>
-            <li class="entidades__item"><?php the_title(); ?></li>
+            <?php $link = get_post_meta(get_the_ID(), '_ungrynerd_link', true); ?>
+            <li class="entidades__item">
+              <?php if (!empty($link)): ?>
+                <a href="<?= $link ?>" target="_blank">
+              <?php endif ?>
+              <?php the_title(); ?>
+              <?php if (!empty($link)): ?>
+                </a>
+              <?php endif ?>
+            </li>
           <?php endwhile; ?>
         </ul>
       </div>
--- wp-content/themes/ffll-foro/lib/meta-boxes.php
+++ wp-content/themes/ffll-foro/lib/meta-boxes.php
@@ -12,25 +12,41 @@ global $meta_boxes;
 $meta_boxes = array();
 
 $meta_boxes[] = array(
-        'id'         => 'general_options',
-        'title'      =>  __('Opciones de post', 'ungrynerd'),
-        'pages'      => array('un_doc' ), // Post type
-        'context'    => 'normal',
-        'priority'   => 'high',
-        'show_names' => true, // Show field names on the left
-        'fields'     => array(
-            array(
-                'id' => $prefix . 'file',
-                'type' => 'file_input',
-                'name' => esc_html__( 'Archivo', 'ungrynerd' ),
-            ),
-            array(
-                    'name' =>  __('Descripción del documento', 'ungrynerd'),
-                    'id' => $prefix . 'desc',
-                    'type' => 'wysiwyg',
-                )
-        ),
-    );
+  'id'         => 'general_options',
+  'title'      =>  __('Enlace', 'ungrynerd'),
+  'pages'      => array('un_entidad' ), // Post type
+  'context'    => 'normal',
+  'priority'   => 'high',
+  'show_names' => true, // Show field names on the left
+  'fields'     => array(
+    array(
+      'id' => $prefix . 'link',
+      'type' => 'url',
+      'name' => esc_html__( 'Enlace', 'ungrynerd' ),
+    )
+  ),
+);
+
+$meta_boxes[] = array(
+  'id'         => 'general_options',
+  'title'      =>  __('Opciones de post', 'ungrynerd'),
+  'pages'      => array('un_doc' ), // Post type
+  'context'    => 'normal',
+  'priority'   => 'high',
+  'show_names' => true, // Show field names on the left
+  'fields'     => array(
+    array(
+      'id' => $prefix . 'file',
+      'type' => 'file_input',
+      'name' => esc_html__( 'Archivo', 'ungrynerd' ),
+    ),
+    array(
+      'name' =>  __('Descripción del documento', 'ungrynerd'),
+      'id' => $prefix . 'desc',
+      'type' => 'wysiwyg',
+    )
+  ),
+);
 
 function ungrynerd_register_meta_boxes()
 {
