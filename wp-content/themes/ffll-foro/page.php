761b04844fe5e755ef24973901ee287756ebe8eb
Daniel Aguilar <dacmail@gmail.com>
2017-08-23 10:39:12 +0200

    All in one

--- wp-content/themes/ffll-foro/page.php
+++ wp-content/themes/ffll-foro/page.php
@@ -1,3 +1,6 @@
 <?php while (have_posts()) : the_post(); ?>
   <?php get_template_part('templates/content', 'page'); ?>
 <?php endwhile; ?>
+<style type="text/css">
+  .page-numbers.current { background-color: #<?php header_textcolor(); ?>;  }
+</style>
