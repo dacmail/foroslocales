c53aecb21c997478e7be9eabd811dc6cd28f16a6
Daniel Aguilar <dacmail@gmail.com>
2018-01-16 14:59:20 +0100

    Eliminación autor en listados general

--- wp-content/themes/ffll-home/templates/components/results.php
+++ wp-content/themes/ffll-home/templates/components/results.php
@@ -9,10 +9,6 @@
     <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
   </h2>
   <div class="results__meta">
-    <div class="results__author">
-      <?= Extras\ungrynerd_svg('icon-author'); ?>
-      <a href="<?= get_author_posts_url(get_the_author_meta('ID')); ?>" rel="author"><?= get_the_author(); ?></a>
-    </div>
     <div class="results__date">
       <?= Extras\ungrynerd_svg('icon-calendar-small'); ?>
       <?php the_time(get_option('date_format')); ?>
