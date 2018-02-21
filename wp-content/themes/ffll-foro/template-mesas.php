6347b38e22b9992e4fde0c714fe7707391d2deb3
Daniel Aguilar <dacmail@gmail.com>
2018-02-21 13:18:59 +0100

    Eliminado el filtro par ano mostrar comisión permanente en listado de mesas

--- wp-content/themes/ffll-foro/template-mesas.php
+++ wp-content/themes/ffll-foro/template-mesas.php
@@ -9,9 +9,8 @@
       </div>
       <div class="col-md-8">
         <p>Si lo necesitas, puedes contactar con cada mesa usando el <a href="<?php echo home_url('/#contacto') ?>">formulario de contacto</a></p>
-        <?php $exclude = get_term_by('slug', 'comision-permanente', 'un_archive'); ?>
         <ul class="entidades">
-          <?php $mesas = get_terms('un_archive', array('hide_empty' => 0, 'exclude' => $exclude->term_id)) ?>
+          <?php $mesas = get_terms('un_archive', array('hide_empty' => 0)) ?>
           <?php foreach ($mesas as $mesa) : ?>
             <li class="entidades__item">
               <a href="<?= get_term_link($mesa, 'un_archive')?>">
